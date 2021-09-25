a958c259ef8ad3c714a9a0b328c0ee4f16a831fa
Kevin <kevinarmandzakka@gmail.com>
2021-09-25 15:57:57 -0700

    Change search strategy for dict.

--- spotlight/parser/dictionary.py
+++ spotlight/parser/dictionary.py
@@ -7,21 +7,35 @@ class DictClientParser(AsyncParser):
     """Performs a dictionary lookup of the input."""
 
     HOSTNAME = "dict.org"
+    DATABASE = "wn"  # Should this be user changeable?
 
     def __init__(self):
         super().__init__()
 
-    def parse_sync(self, string: str) -> str:
-        client = DictionaryClient(self.HOSTNAME)
-        definition = client.define(string).content
-        if definition is None:
-            return f"No definitions found for {string}."
-        ret = ""
-        for defin in definition:
-            ret += f"""
-            From {client.databases[defin['db']]}:
+    def run_query(self, query: str):
+        super().run_query(query)
+        return "Searching definition..."
 
-            {defin['definition']}
-            """
+    def parse_sync(self, string: str) -> str:
+        try:
+            client = DictionaryClient(self.HOSTNAME)
+
+            definition = client.define(string, db=self.DATABASE).content
+            if definition is None:
+                matches = client.match(string, db=self.DATABASE).content[self.DATABASE]
+                if len(matches) > 0:
+                    definition = client.define(matches[0], self.DATABASE).content
+        except:
+            return "Connection error..."
+
+        if definition is None:
+            answer = f"No match found for {string}."
+        else:
+            answer = self._format_answer(definition)
+
         client.disconnect()
-        return ret
+        return answer
+
+    def _format_answer(self, definition) -> str:
+        assert len(definition) == 1
+        return f"{definition[0]['definition']}"
