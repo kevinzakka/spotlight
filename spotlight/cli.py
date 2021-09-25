752e55c1c9a51d4d7ff6dff716ecfb315d6ed9e2
Alex Nichol <unixpickle@gmail.com>
2021-09-25 16:29:29 -0400

    callback works

--- spotlight/cli.py
+++ spotlight/cli.py
@@ -9,6 +9,15 @@ from .spotlight import Spotlight
 
 
 def main():
-    app = Spotlight()
-    app.show_all()
+    from .parser.base import SleepyParrotParser
+
+    parser = SleepyParrotParser()
+    parser.connect("query_result", handle_result)
+    parser.run_query("hi")
+    # app = Spotlight()
+    # app.show_all()
     Gtk.main()
+
+
+def handle_result(_, query_output):
+    print("hi", query_output)
--- spotlight/parser/base.py
+++ spotlight/parser/base.py
@@ -2,11 +2,12 @@ import abc
 
 import time
 import threading
+from typing import Tuple
 
 import gi
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import GObject
+from gi.repository import GObject, GLib
 
 
 class Parser(abc.ABC):
@@ -20,23 +21,24 @@ class Parser(abc.ABC):
         """Parse a string."""
 
 
-class AsyncParser(GObject):
+class AsyncParser(GObject.Object):
     """Base async parser abstraction."""
 
     def __init__(self):
+        super().__init__()
         self._cond = threading.Condition()
         self._query = None
 
-        thread = threading.Thread(target=self.run, args=(), daemon=True)
+        thread = threading.Thread(target=self._run, args=(), daemon=True)
         thread.start()
 
     @GObject.Signal(
         flags=GObject.SignalFlags.RUN_LAST,
         return_type=bool,
-        arg_types=(str, str),
+        arg_types=(object,),
         accumulator=GObject.signal_accumulator_true_handled,
     )
-    def query_result(query: str, value: str):
+    def query_result(self, query_and_value: Tuple[str, str]):
         pass
 
     def run_query(self, query: str):
@@ -50,26 +52,25 @@ class AsyncParser(GObject):
                 if self._query is None:
                     self._cond.wait()
                 query = self._query
-            result = self.parse(query)
-            with self.cond:
+            result = self.parse_sync(query)
+            with self._cond:
                 # Don't trigger signal if query changed.
-                if self.query == query:
-                    self.emit_async("query_result", query, result)
-                    self.query = None
+                if self._query == query:
+                    ev = GLib.Idle()
+                    ev.set_callback(
+                        lambda *args: self.emit("query_result", (query, result))
+                    )
+                    ev.attach(GLib.MainContext.default())
+                    self._query = None
 
     @abc.abstractmethod
-    def parse(self, string: str) -> str:
+    def parse_sync(self, string: str) -> str:
         """Parse a string."""
 
 
 class SleepyParrotParser(AsyncParser):
     """A parser that sleeps then repeats what it was fed."""
 
-    def parse(self, string: str) -> str:
-        self.string = string
-        return "Looking up term..."
-
-    def _parse(self):
+    def parse_sync(self, string: str) -> str:
         time.sleep(0.1)
-        self.ret = self.string
-        self.string = None
+        return "hello " + string
