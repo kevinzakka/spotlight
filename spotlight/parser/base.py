445e3ce8ae0505b3e1b6aefffb8a7c0ce4afd664
Alex Nichol <unixpickle@gmail.com>
2021-09-25 16:19:59 -0400

    attempt at base class

--- spotlight/parser/base.py
+++ spotlight/parser/base.py
@@ -3,6 +3,11 @@ import abc
 import time
 import threading
 
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import GObject
+
 
 class Parser(abc.ABC):
     """Base parser abstraction."""
@@ -15,29 +20,46 @@ class Parser(abc.ABC):
         """Parse a string."""
 
 
-class AsyncParser(abc.ABC):
+class AsyncParser(GObject):
     """Base async parser abstraction."""
 
     def __init__(self):
-        self.string = None
-        self.ret = None
+        self._cond = threading.Condition()
+        self._query = None
 
-        thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True
+        thread = threading.Thread(target=self.run, args=(), daemon=True)
         thread.start()
 
-    def run(self):
+    @GObject.Signal(
+        flags=GObject.SignalFlags.RUN_LAST,
+        return_type=bool,
+        arg_types=(str, str),
+        accumulator=GObject.signal_accumulator_true_handled,
+    )
+    def query_result(query: str, value: str):
+        pass
+
+    def run_query(self, query: str):
+        with self._cond:
+            self._query = query
+            self._cond.notify_all()
+
+    def _run(self):
         while True:
-            if self.string is not None:
-                self._parse()
-            time.sleep(0.05)
+            with self._cond:
+                if self._query is None:
+                    self._cond.wait()
+                query = self._query
+            result = self.parse(query)
+            with self.cond:
+                # Don't trigger signal if query changed.
+                if self.query == query:
+                    self.emit_async("query_result", query, result)
+                    self.query = None
 
     @abc.abstractmethod
     def parse(self, string: str) -> str:
         """Parse a string."""
-
-    def _parse(self):
-        pass
 
 
 class SleepyParrotParser(AsyncParser):
