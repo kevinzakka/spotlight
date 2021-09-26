2fd4fc179834203a3ecb080ad6fe0c6706a08e9c
Kevin <kevinarmandzakka@gmail.com>
2021-09-26 12:22:36 -0700

    Keep the dictionary connection alive to make subsequent searches faster.
    Unsure if this is good practice however, and I am not explicitly closing
    the connection, instead relying on the program exiting to do this.

--- spotlight/parser/dictionary.py
+++ spotlight/parser/dictionary.py
@@ -9,28 +9,36 @@ class DictClientParser(AsyncParser):
     HOSTNAME = "dict.org"
     DATABASE = "wn"  # Should this be user changeable?
 
+    def __init__(self):
+        super().__init__()
+
+        self.client = None
+
     def run_query(self, query: str):
         super().run_query(query)
 
         return "Searching definition..."
 
     def parse_sync(self, string: str) -> str:
-        client = DictionaryClient(self.HOSTNAME)
-        definition = client.define(string, db=self.DATABASE).content
+        if self.client is None:
+            self.client = DictionaryClient(self.HOSTNAME)
+
+        definition = self.client.define(string, db=self.DATABASE).content
         if not definition:
-            matches = client.match(string, db=self.DATABASE).content
+            matches = self.client.match(string, db=self.DATABASE).content
             if matches is None:
                 definition = None
             else:
                 if len(matches) > 0:
-                    definition = client.define(
+                    definition = self.client.define(
                         matches[self.DATABASE][0], self.DATABASE
                     ).content
+
         if definition is None:
             answer = f"No match found for {string}."
         else:
             answer = self._format_answer(definition)
-        client.disconnect()
+
         return answer
 
     def _format_answer(self, definition) -> str:
